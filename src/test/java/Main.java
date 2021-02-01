1b8dbe5747d8bc5aabd8e5277ed153df85d9f58e
utk <utk.priyam003@gmail.com>
2021-02-01 12:15:10 -0800

    added annotations

--- src/test/java/Main.java
+++
@@ -1,8 +0,0 @@
-import io.github.utk003.util.function.lambda.*;
-
-public class Main {
-    public static void main(String[] args) {
-        Lambda3<Integer, Integer, Integer, Integer> lambda = (Integer i1, Integer i2, Integer i3) -> i1 + i2 + i3;
-        System.out.println(lambda.get(5,6,7));
-    }
-}
