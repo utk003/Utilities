f2ed0fb53dee2a4b8d506a3f5328e92fc81af414
utk <utk.priyam003@gmail.com>
2021-02-21 14:49:52 -0800

    v1.1.0

--- src/main/java/io/github/utk003/util/misc/Verify.java
+++ src/main/java/io/github/utk003/util/misc/Verify.java
@@ -32,150 +32,215 @@ import org.jetbrains.annotations.Nullable;
  * <p>
  * This class provides these methods:
  * <ul>
- * <li>The {@code requireTrue} methods, which throw an {@link IllegalArgumentException}
+ * <li>The {@code fail} methods, which always throw a {@link VerificationException}.
+ * <li>The {@code requireTrue} methods, which throw a {@link VerificationException}
  *     if and only if the given boolean expression evaluates to {@code false}.
- * <li>The {@code requireFalse} methods, which throw an {@link IllegalArgumentException}
+ * <li>The {@code requireFalse} methods, which throw a {@link VerificationException}
  *     if and only if the given boolean expression evaluates to {@code true}.
- * <li>The {@code requireNull} methods, which throw an {@link IllegalArgumentException}
+ * <li>The {@code requireNull} methods, which throw a {@link VerificationException}
  *     if and only if the given object expression is not {@code null}.
- * <li>The {@code requireNotNull} methods, which throw an {@link IllegalArgumentException}
+ * <li>The {@code requireNotNull} methods, which throw a {@link VerificationException}
  *     if and only if the given object expression is {@code null}.
  * </ul>
  *
  * @author Utkarsh Priyam (<a href="https://github.com/utk003" target="_top">utk003</a>)
- * @version January 13, 2021
+ * @version February 21, 2021
  * @since 1.0.2
  */
+// TODO The {@code requireMatch} methods, which throw a {@link VerificationException}
+//  if and only if the given objects or primitives do not match.
 public abstract class Verify {
     private Verify() {
     }
 
     /**
-     * Throws an {@link IllegalArgumentException} if and only if the given
-     * boolean argument is not {@code true}.
-     * <p>
-     * If an exception is thrown, it is initialized with a {@code null} message.
+     * Throws a {@link VerificationException}
      *
-     * @param bool The boolean argument to check
-     * @throws IllegalArgumentException If the boolean argument is not {@code true}
-     * @see #requireTrue(boolean, String)
+     * @throws VerificationException Always
+     * @since 1.1.0
+     */
+    @Contract(" -> fail")
+    public static void fail() {
+        throw new VerificationException();
+    }
+    /**
+     * Throws a {@link VerificationException} with the specified message.
+     *
+     * @param message The exception's message
+     * @throws VerificationException Always
+     * @since 1.1.0
+     */
+    @Contract("_ -> fail")
+    public static void fail(String message) {
+        throw new VerificationException(message);
+    }
+
+    /**
+     * Throws a {@link VerificationException} (with no message)
+     * if and only if the given boolean is not {@code true}.
+     *
+     * @param bool The boolean to check
+     * @throws VerificationException If the boolean is not {@code true}
      */
     @Contract("false -> fail")
     public static void requireTrue(boolean bool) {
-        requireTrue(bool, null);
+        if (!bool) fail();
     }
     /**
-     * Throws an {@link IllegalArgumentException} if and only if the given
-     * boolean argument is not {@code true}.
-     * <p>
-     * If an exception is thrown, it is initialized with the specified message.
+     * Throws an {@link VerificationException} (with the specified message)
+     * if and only if the given boolean is not {@code true}.
      *
-     * @param bool    The boolean argument to check
+     * @param bool    The boolean to check
      * @param message The message for the exception
-     * @throws IllegalArgumentException If the boolean argument is not {@code true}
+     * @throws VerificationException If the boolean is not {@code true}
      */
     @Contract("false,_ -> fail")
     public static void requireTrue(boolean bool, String message) {
-        if (!bool)
-            throw new IllegalArgumentException(message);
+        if (!bool) fail(message);
     }
 
     /**
-     * Throws an {@link IllegalArgumentException} if and only if the given
-     * boolean argument is not {@code false}.
-     * <p>
-     * If an exception is thrown, it is initialized with a {@code null} message.
+     * Throws an {@link VerificationException} (with no message)
+     * if and only if the given boolean is not {@code false}.
      *
-     * @param bool The boolean argument to check
-     * @throws IllegalArgumentException If the boolean argument is not {@code false}
-     * @see #requireFalse(boolean, String)
+     * @param bool The boolean to check
+     * @throws VerificationException If the boolean is not {@code false}
      */
     @Contract("true -> fail")
     public static void requireFalse(boolean bool) {
-        requireFalse(bool, null);
+        if (bool) fail();
     }
     /**
-     * Throws an {@link IllegalArgumentException} if and only if the given
-     * boolean argument is not {@code false}.
-     * <p>
-     * If an exception is thrown, it is initialized with the specified message.
+     * Throws an {@link VerificationException} (with the specified message)
+     * if and only if the given boolean is not {@code false}.
      *
-     * @param bool    The boolean argument to check
+     * @param bool    The boolean to check
      * @param message The message for the exception
-     * @throws IllegalArgumentException If the boolean argument is not {@code false}
+     * @throws VerificationException If the boolean is not {@code false}
      */
     @Contract("true,_ -> fail")
     public static void requireFalse(boolean bool, String message) {
-        if (bool)
-            throw new IllegalArgumentException(message);
+        if (bool) fail(message);
     }
 
     /**
-     * Throws an {@link IllegalArgumentException} if and only if the given
-     * {@code Object} argument is not {@code null}.
-     * <p>
-     * If an exception is thrown, it is initialized with a {@code null} message.
+     * Throws an {@link VerificationException} (with no message)
+     * if and only if the given object is not {@code null}.
      *
-     * @param obj The {@code Object} argument to check
-     * @throws IllegalArgumentException If the {@code Object} argument is not {@code null}
-     * @see #requireNull(Object, String)
+     * @param obj The object to check
+     * @throws VerificationException If the object is not {@code null}
      */
     @Contract("!null -> fail")
     public static void requireNull(@Nullable Object obj) {
-        requireNull(obj, null);
+        if (obj != null) fail();
     }
     /**
-     * Throws an {@link IllegalArgumentException} if and only if the given
-     * {@code Object} argument is not {@code null}.
-     * <p>
-     * If an exception is thrown, it is initialized with the specified message.
+     * Throws an {@link VerificationException} (with the specified message)
+     * if and only if the given object is not {@code null}.
      *
-     * @param obj     The {@code Object} argument to check
+     * @param obj     The object to check
      * @param message The message for the exception
-     * @throws IllegalArgumentException If the {@code Object} argument is not {@code null}
+     * @throws VerificationException If the object is not {@code null}
      */
     @Contract("!null,_ -> fail")
     public static void requireNull(@Nullable Object obj, String message) {
-        if (obj != null)
-            throw new IllegalArgumentException(message);
+        if (obj != null) fail(message);
     }
 
     /**
-     * Throws an {@link IllegalArgumentException} if and only if the given
-     * {@code Object} argument is not {@code not-null} or equivalently is {@code null}.
+     * Throws an {@link VerificationException} (with no message) if and only if
+     * the given object is not {@code not-null} (or, equivalently, is {@code null}).
      * <p>
-     * If an exception is thrown, it is initialized with a {@code null} message.
-     * <p>
-     * For a method with identical functionality that returns the argument as well,
+     * For a method with nearly-identical functionality that returns the argument as well,
      * use {@link java.util.Objects#requireNonNull(Object)} instead.
      *
      * @param obj The {@code Object} argument to check
-     * @throws IllegalArgumentException If the {@code Object} argument is {@code null}
-     * @see #requireNull(Object, String)
+     * @throws VerificationException If the {@code Object} argument is {@code null}
      * @see java.util.Objects#requireNonNull(Object)
      */
     @Contract("null -> fail")
     public static void requireNotNull(@Nullable Object obj) {
-        requireNotNull(obj, null);
+        if (obj == null) fail();
     }
     /**
-     * Throws an {@link IllegalArgumentException} if and only if the given
-     * {@code Object} argument is not {@code not-null} or equivalently is {@code null}.
+     * Throws an {@link VerificationException} (with the specified message) if and only if
+     * the given object is not {@code not-null} (or, equivalently, is {@code null}).
      * <p>
-     * If an exception is thrown, it is initialized with the specified message.
-     * <p>
-     * For a method with identical functionality that returns the argument as well,
+     * For a method with nearly-identical functionality that returns the argument as well,
      * use {@link java.util.Objects#requireNonNull(Object, String)} instead.
      *
      * @param obj     The {@code Object} argument to check
      * @param message The message for the exception
-     * @throws IllegalArgumentException If the {@code Object} argument is {@code null}
-     * @see #requireNull(Object, String)
+     * @throws VerificationException If the {@code Object} argument is {@code null}
      * @see java.util.Objects#requireNonNull(Object, String)
      */
     @Contract("null,_ -> fail")
     public static void requireNotNull(@Nullable Object obj, String message) {
-        if (obj == null)
-            throw new IllegalArgumentException(message);
+        if (obj == null) fail(message);
+    }
+
+    /**
+     * A custom runtime exception for use by the
+     * {@link Verify} class for failed verifications.
+     *
+     * @author Utkarsh Priyam (<a href="https://github.com/utk003" target="_top">utk003</a>)
+     * @version February 21, 2021
+     * @since 1.1.0
+     */
+    public static class VerificationException extends RuntimeException {
+        /**
+         * Constructs a new verification exception
+         *
+         * @see RuntimeException#RuntimeException()
+         */
+        public VerificationException() {
+            super();
+        }
+
+        /**
+         * Constructs a new runtime exception with the specified detail message
+         *
+         * @param message The detail message
+         * @see RuntimeException#RuntimeException(String)
+         */
+        public VerificationException(String message) {
+            super(message);
+        }
+
+        /**
+         * Constructs a new runtime exception with the specified detail message and cause
+         *
+         * @param message The detail message
+         * @param cause   The cause
+         * @see RuntimeException#RuntimeException(String, Throwable)
+         */
+        public VerificationException(String message, Throwable cause) {
+            super(message, cause);
+        }
+
+        /**
+         * Constructs a new runtime exception with the specified cause
+         *
+         * @param cause The cause
+         * @see RuntimeException#RuntimeException(Throwable)
+         */
+        public VerificationException(Throwable cause) {
+            super(cause);
+        }
+
+        /**
+         * Constructs a new runtime exception with the specified detail
+         * message, cause, suppression enabled or disabled, and writable
+         * stack trace enabled or disabled
+         *
+         * @param message            The detail message
+         * @param cause              The cause
+         * @param enableSuppression  Whether or not suppression is enabled or disabled
+         * @param writableStackTrace Whether or not the stack trace should be writable
+         * @see RuntimeException#RuntimeException(String, Throwable, boolean, boolean)
+         */
+        protected VerificationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+            super(message, cause, enableSuppression, writableStackTrace);
+        }
     }
 }
